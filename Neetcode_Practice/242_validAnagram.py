ed817331bb142ecd2b6dc1ca17e9f9f22688a831
YileChoi <choiyile@gmail.com>
2022-10-08 15:48:03 -0400

    Update 242_validAnagram.py
    My own solution version of checking anagram

--- Neetcode_Practice/242_validAnagram.py
+++ Neetcode_Practice/242_validAnagram.py
@@ -1,23 +1,22 @@
-class Solution:
+class Solution(object):
     def isAnagram(self, s, t):
-        # check if both strs of same length
-        if len(s) != len(t):
-            return False
-        
-        # declare an empty dictionary to keep track of alphabet count
-        count_s = {}
-        
-        # Insert an element to the dict if not available otherwise increase the count of element
-        for i in range(len(s)):
-            count_s[s[i]] = 1 + count_s.get(s[i], 0)
-        
-        # Check if char in t is available in s
-        for i in t:
-            if i in count_s:
-                count_s[i] = count_s[i] - 1
-        
-        # If all the values in the dictionary count_s is 0, it means t is an anagram of s
-        for val in count_s.values():
-            if val != 0:
-                return False
-        return True
+      result = False
+      anagram_list1 = []
+      anagram_list2 = []
+      if len(s) != len(t):
+        return False
+      else:
+        for i in range(0, len(s)):
+          anagram_list1 += s[i]
+          anagram_list2 += t[i]
+
+        anagram_list1.sort()
+        anagram_list2.sort()
+
+        for i in range(0, len(anagram_list1)):
+          if anagram_list1[i] != anagram_list2[i]:
+            result = False
+            break
+          else:
+            result = True
+      return result
